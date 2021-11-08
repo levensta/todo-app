99005d37ffcfb6d0625dad946f82da8e2a8a3cc4
levensta <levensta@student.21-school.ru>
2021-11-08 23:32:49 +0300

    add popup

--- src/components/AddList/index.jsx
+++ src/components/AddList/index.jsx
@@ -2,17 +2,19 @@ import React, { useState } from "react";
 import List from "../List";
 import Badge from '../Badge';
 
+import closeSvg from "../../assets/img/close.svg"
 import "./AddList.scss"
 
-const AddList = ( {colors}) => {
+const AddList = ( {colors} ) => {
 	// значение по умолчанию – false
-	const [visiblePopup, setVisiblePopup] = useState(true);
+	const [visiblePopup, setVisiblePopup] = useState(false);
+	const [selectedColor, selectColor] = useState(colors[0].id);
 	return (
 		// react не может отрендерить сразу два элемента, поэтому им обязательно иметь родительский
 		// чтобы не плодить лишние div'ы внутри друг друга, можно использовать React.Fragment
 		<div className="add-list">
 			<List  
-				oncClick={() => setVisiblePopup(true)}
+				onClick={() => setVisiblePopup(true)}
 				items={[
 					{
 						className: "list__add-button",
@@ -23,16 +25,29 @@ const AddList = ( {colors}) => {
 						</svg>
 						),
 						name: "Добавить список"
-					},
+					}
 				]}
 			/>
 			{visiblePopup && (
 				<div className="add-list__popup">
+					<img
+						onClick={() => setVisiblePopup(false)}
+						src={closeSvg}
+						alt="close icon"
+						className="add-list__popup-close-btn"
+					/>
 					<input className="field" type="text" placeholder="Название списка" />
 					<div className="add-list__popup-colors">
-						<ul>
-							<Badge color="pink" />
-						</ul>
+						{
+							colors.map(color => (
+								<Badge
+									onClick={() => selectColor(color.id)}
+									key={color.id}
+									color={color.name}
+									className={selectedColor === color.id && "active" }	
+								/>)
+							)
+						}
 					</div>
 					<button className="button">Добавить список</button>
 				</div>
