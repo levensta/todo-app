cae785cbc102789aad4e710cf17320e0953ae522
levensta <levensta@student.21-school.ru>
2021-11-11 04:01:11 +0300

    update Task component

--- src/components/Tasks/index.jsx
+++ src/components/Tasks/index.jsx
@@ -6,20 +6,22 @@ import "./Tasks.scss";
 const Tasks = () => {
 	return (
 		<div className="tasks">
-			<h2 className={"tasks__title"}>
+			<h2 className="tasks__title">
 				Frontend
 				<img src={editSvg} alt="edit icon" />
 			</h2>
-			<div className="task__items">
-				{/* имитация чекбокса на отдельный элемент */}
-				<div className="checkbox">
-					<input id="check" type="checkbox"/>
+			<div className="tasks__items">
+				<div className="tasks__items-row">
+					<div className="checkbox">
+						<input id="check" type="checkbox"/>
+						<label htmlFor="check" >
+							<svg width="11" height="8" viewBox="0 0 11 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<path d="M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+							</svg>
+						</label>
+					</div>
+					<input value="ReactJS (hooks, props, state)" />
 				</div>
-				<label htmlFor="check" >
-					<svg width="11" height="8" viewBox="0 0 11 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path d="M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-					</svg>
-				</label>
 			</div>
 		</div>
 	);
